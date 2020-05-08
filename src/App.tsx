c84f1bc96678cdc20d64534e9f086dbd0e20ef21
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-08 19:04:04 +0100

    Functional/stateless component syntax.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from "./Message"
 
-// Tuple:
-let aTuple: [string, number] = ["Alican", 21];
-// Enum:
-enum Codes {first = 1, second = 2}
-// Any:
-let firstName: any = "Alican"
-// Void:
-const warning = (): void => console.log("Warning.");
-
-let App = () =>
+const App = () =>
     <div className="App">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>The value {firstName} is of {typeof firstName} type!</p>
+            <Message />
         </header>
     </div>
+
 
 
 export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+
+const Message = () => <p>This is a quick message!</p>
+
+export default Message;
+
