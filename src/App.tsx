a687e18303ddd161c567028691fbb7430aa0e398
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-08 17:21:53 +0100

    Other complex types.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// let firstValue: string = "Manny";
-// let firstValue: number = 34;
-// let firstValue: boolean = true;
-// let firstValue: number[] = [ 2, 3, 5, 6 ];
-let firstValue: Array<string> = [ '2', '3', 'Manny' ];
+// Tuple:
+let aTuple: [string, number] = ["Alican", 21];
+// Enum:
+enum Codes {first = 1, second = 2}
+// Any:
+let firstName: any = "Alican"
+// Void:
+const warning = (): void => console.log("Warning.");
 
-let App = () => 
+let App = () =>
     <div className="App">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                The value {firstValue} is of {typeof firstValue} type!
-            </p>
+            <p>The value {firstName} is of {typeof firstName} type!</p>
         </header>
     </div>
+
 
 export default App;
