93e918a09b961117e766a3c7013ac510d8aa21de
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-08 16:56:25 +0100

    Basic types.

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// let firstValue: string = "Manny";
+// let firstValue: number = 34;
+// let firstValue: boolean = true;
+// let firstValue: number[] = [ 2, 3, 5, 6 ];
+let firstValue: Array<string> = [ '2', '3', 'Manny' ];
+
+let App = () => 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                The value {firstValue} is of {typeof firstValue} type!
+            </p>
+        </header>
     </div>
-  );
-}
 
 export default App;
