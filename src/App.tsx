f27ed22c8eff315e76fe61baef2e34cbf23bd9f4
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-08 19:24:39 +0100

    Class/stateful component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from "./Message"
 
-const App = () =>
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Message message="This is a simple message."/>
-        </header>
-    </div>
+class App extends Component<any> {
+    componentWillMount = () => console.log("Almost there...");
+    componentDidMount = () => console.log("Finally. Hello.");
+    render = () =>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Message message="This is a simple message."/>
+            </header>
+        </div>
+}
 
 export default App;
